8ba8393165a94cc0785bf08a706950a4b7d20d28
JuanKa Lagoria <lagoria79@gmail.com>
2024-11-22 18:00:05 -0300

    First commit for the project

---
+++ backend-service/omdb-api-service/src/main/java/com/omdb/api/OmdbApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.omdb.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmdbApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmdbApiServiceApplication.class, args);
+	}
+
+}
